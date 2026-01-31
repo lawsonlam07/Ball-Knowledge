366595f24aacb26570094cc4d6c8fb163b7df0f5
hjc25 <hjc25@ic.ac.uk>
2026-01-31 21:17:23 +0000

    Full pipeline

--- logic/pipeline.py
+++ logic/pipeline.py
@@ -1,16 +1,18 @@
 from data.eventframe import EventFrame
 from data.frame import Frame
 from data.framestack import FrameStack
+from data.orderofevents import OrderOfEvents
 from logic.events import EventTesters
 from logic.perspective import FrameUnskew
 from vision import VisionSystem, get_court_calibration
 
 
 def process_frames():
-    fps = 30
+    fps = 60
     system = VisionSystem("tennis2.mp4")
     stack = FrameStack(60)
     i = 0
+    order = OrderOfEvents()
     while True:
         i = i + 1
         frame: Frame = system.getNextFrame()
@@ -25,12 +27,10 @@ def process_frames():
             result = tester.test_event(stack)
             if result is not None:
                 # Pass events for which the test passes to event frames
-                results.append(EventFrame(frame, result.to_string()))
+                order.addEvent(EventFrame(frame, result.to_string()))
         # Extract the event strings from the EventFrame objects
         event_descriptions = [res.event for res in results]
         print(f"Frame {i}: {' | '.join(event_descriptions)}")
 
-        # Push to the event ordering
-
-    # Merge consecutive events
-    # Pass
+        # Merge consecutive events
+        order.mergeConsecutiveEvents()
